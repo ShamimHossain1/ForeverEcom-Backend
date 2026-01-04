7eaf25c73e4cbb19c4a74176d475e23131894a4a
ShamimHossain1 <shamimweb78@gmail.com>
2026-01-04 09:57:58 -0500

    implement user and admin authentication routes in userController

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+// routes for user login
+
+const loginUser = async(req, res) => { 
+}
+
+// routes for user signup
+
+const registerUser = async(req, res) => { 
+}
+
+// Route for admin login
+const loginAdmin = async(req, res) => { 
+}
+
+
+module.exports = { loginUser, registerUser, loginAdmin }    
