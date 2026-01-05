508eda8216072e06eea88c77c1d8a49722b2a2db
ShamimHossain1 <shamimweb78@gmail.com>
2026-01-05 12:35:45 -0500

    implement user registration logic with validation and password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,13 @@
 // routes for user login
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel";
+
+const createToke = (userId) => { 
+        return jwt.sign({id}, )
+}
+
 
 const loginUser = async(req, res) => { 
 }
@@ -6,7 +15,43 @@ const loginUser = async(req, res) => {
 // routes for user signup
 
 const registerUser = async(req, res) => { 
-    res.json({msg:"Register api working"})
+   try{
+    const{name, email, password} = req.body;
+
+    // checking user already exist or not
+    const existUser = await userModel.findOne({email});
+
+    if(existUser){
+        return res.status(400).json({success: false, message: "User already exist"});
+    }
+
+    // validation email format and strong password
+
+    if(!validator.isEmail(email)){
+        return res.status(400).json({success: false, message: "Invalid email format"});
+    }
+    if(password.length < 6){
+        return res.status(400).json({success: false, message: "Password must be at least 6 characters"});
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json({success: true, message: "User registered successfully", user});
+
+    const token = 
+
+   }catch(err){
+    console.log(err);
+   }
 }
 
 // Route for admin login
